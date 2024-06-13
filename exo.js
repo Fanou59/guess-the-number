620616b7069a354b31df44402c9aa5a62ffa4b15
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2024-06-13 12:42:17 +0200

    1er étape du JS

--- exo.js
+++ exo.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// Variables de récupération des éléments du DOM
+const inputGuess = document.querySelector("#guess");
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault;
+  const numberGuess = inputGuess.value;
+  console.log(numberGuess);
+});
+// générer un nombre entier aléatoire entre 0 et 500
+// récupérer le nombre proposé par l'utilsateur après le submit
+// écrire la condition si le chiffre est au dessus ou en dessous
+// en fonction de la réponse : afficher 🔴 ou 🟢
+// en fonction de la réponse : afficher soit le chiffre proposé dans 'My guess is' + below or above + chiffre
+// afficher une croix dans l'input entre 0 et 500 en fonction du chiffre proposé
+// afficher 🟢 dans l'input si le chiffre est bon
+// incrémenter le score de +1 à chaque tentative
+// refresh du jeu (clear) en cliquant sur REPLAY
